424fb7e7a655f25a81e7ce67c40cc4e408168502
Enrique Peñaloza <epenaloza@novopayment.com>
2019-02-12 11:51:23 -0400

    Icorpora registro y login

--- server/start/routes.js
+++ server/start/routes.js
@@ -19,3 +19,8 @@ const Route = use('Route')
 Route.get('/', () => {
   return { greeting: 'Hello world in JSON' }
 })
+
+Route.group(() => {
+  Route.post('auth/register', 'UserController.register')
+  Route.get('auth/login', 'UserController.login')
+}).prefix('api/v1')
